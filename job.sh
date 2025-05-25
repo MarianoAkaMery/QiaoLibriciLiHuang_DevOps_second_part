34c7a0b043a339768ae2c7f1909ff0dd456dd03e
MarianoAkaMery <marianolibrici2002@gmail.com>
2025-05-25 22:17:51 +0000

    Final Push for Task 2/3

--- job.sh
+++ job.sh
@@ -11,22 +11,22 @@
 module load singularity
 
 export TMPDIR=~/tmp
-mkdir -p "$TMPDIR"
+mkdir -p $TMPDIR
 
-export OMPI_MCA_tmpdir_base="$TMPDIR"
-export OMPI_MCA_orte_tmpdir_base="$TMPDIR"
+export OMPI_MCA_tmpdir_base=$TMPDIR
+export OMPI_MCA_orte_tmpdir_base=$TMPDIR
 export OMPI_MCA_plm_rsh_agent="ssh :rsh"
 export OMPI_MCA_btl=self,tcp
 
-export SINGULARITY_TMPDIR="$TMPDIR/singularity_tmp"
-export SINGULARITY_CACHEDIR="$TMPDIR/singularity_cache"
-mkdir -p "$SINGULARITY_TMPDIR" "$SINGULARITY_CACHEDIR"
+export SINGULARITY_TMPDIR=$TMPDIR/singularity_tmp
+export SINGULARITY_CACHEDIR=$TMPDIR/singularity_cache
+mkdir -p $SINGULARITY_TMPDIR $SINGULARITY_CACHEDIR
 
 echo "Create tmp directory SINGULARITY_TMPDIR = $SINGULARITY_TMPDIR"
 echo "Create cache directory SINGULARITY_CACHEDIR = $SINGULARITY_CACHEDIR"
 
 echo "Running grayscale conversion..."
-singularity exec grayscale.sif /opt/app/build/convert_grayscale input output Average
+singularity exec --bind $HOME/seproject:/opt/app_src grayscale.sif /opt/app/build/convert_grayscale input output Average
 
 echo "Running grayscale tests..."
-singularity exec grayscale.sif /opt/app/build/test_grayscale >> grayscale_output.log 2>&1
+singularity exec --bind $HOME/seproject:/opt/app_src grayscale.sif /opt/app/build/test_grayscale >> grayscale_output.log 2>&1
