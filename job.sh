92d3f2657826629cf6197e205345385981781dc4
MarianoAkaMery <marianolibrici2002@gmail.com>
2025-05-26 01:42:17 +0000

    Final Push for Task 2/3 Back

--- job.sh
+++ job.sh
@@ -8,16 +8,16 @@
 module purge
 module load singularity 2>/dev/null || true
 
-# --- percorsi sul filesystem del nodo ---------------------------------
+# --- percorsi sul filesystem del nodo ----------------------------------
 IN_HOST="$SCRATCH/seproject/images"      # cartella con le immagini INPUT
 OUT_HOST="$SCRATCH/seproject/result"     # cartella per salvare l’OUTPUT
 SIF="$HOME/seproject/grayscale.sif"      # container copiato dallo script CI
 
 mkdir -p "$OUT_HOST"
 
-# --- esegui il container montando le directory host -------------------
+# --- esegui il container montando le directory host ---------------------
 singularity exec \
   --bind "${IN_HOST}:/data/input" \
   --bind "${OUT_HOST}:/data/output" \
   "$SIF" \
-  /usr/local/bin/convert_grayscale /data/input /data/output average
+  /usr/local/bin/convert_grayscale /data/input /data/output Average
